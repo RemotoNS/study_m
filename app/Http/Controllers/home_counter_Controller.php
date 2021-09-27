f9861b1e07cb6aeda1f68b3945111709266ed389
Okamoto Kazuki <okamotokazuki@OkamotonoMacBook-Air.local>
2021-09-27 09:26:53 +0900

    ”home_login_count”

--- app/Http/Controllers/home_counter_Controller.php
+++ app/Http/Controllers/home_counter_Controller.php
@@ -8,7 +8,6 @@ class home_counter_Controller extends Controller
 {
     public function getIndex()
 {
-    // セッション値「count」の存在確認
     if(session()->has('count')){
         // あり
         $count = session('count');
