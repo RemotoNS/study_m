eaa63d06e059bb8dedf0a7105809dede3cbc28ff
Okamoto Kazuki <okamotokazuki@OkamotonoMacBook-Air.local>
2021-09-28 12:32:29 +0900

    ”design_repair”

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -5,6 +5,13 @@
 @section("content")
 
 <p>今回のポモドーロテクニック</p>
-<p><font size = "100" color = "red">{{ $count }}</font></p>
+<p><font size = "100" color = "red">{{ $count -1 }}回達成！</font></p>
+
+<form method="POST" action="#">
+@csrf
+    <input type="hidden" name="count" value="{{ $count -1 }}">
+    <p>ニックネーム；<input type="name" name="name">氏</p>
+    <input type="submit" value="登録">
+</form>
 
 @endsection
