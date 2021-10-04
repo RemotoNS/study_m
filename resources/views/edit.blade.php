c7d4cb726d52364d0cc1aa63a2248051c5d123aa
Okamoto Kazuki <okamotokazuki@OkamotonoMacBook-Air.local>
2021-10-04 16:00:40 +0900

    ””

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,19 +4,45 @@
 
 @section("content")
 
-<p>投稿ページ</p>
-<form action="/edit_post" method="POST">
-@csrf
-    <label>ジャンルセレクト
-        <select name="category_id">
-            <option value="1">食事</option>
-            <option value="2">運動</option>
-            <option value="3">睡眠</option>
-        </select>
-    </label>
 
-    <p>タイトル；<input type="name" name="title" value="{{ $article -> title }}"></p>
-    <p>内容；<input type="text" name="content" value="{{ $article -> content }}"></p>
+<p>投稿ページ</p>
+
+<div class="card-body">
+<form action="/edit_post" method="POST">
+    @csrf
+    <div class="form-group row">
+        <label>ジャンルセレクト
+            <select name="category_id">
+                <option value="1">食事</option>
+                <option value="2">運動</option>
+                <option value="3">睡眠</option>
+            </select>
+        </label>
+    </div>
+
+    <div class="form-group row">
+        <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('タイトル') }}</label>
+        <div class="col-md-6">
+            <input type="text" class="form-control @error('taxt') is-invalid @enderror" name="title" value="{{ $article -> title }}" required autocomplete="taxt" autofocus >
+                @error('text')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="content" class="col-md-4 col-form-label text-md-right">{{ __('内容') }}</label>
+        <div class="col-md-6">
+            <input type="text" class="form-control @error('content') is-invalid @enderror" name="content" value="{{ $article -> content }}" required autocomplete="taxt" autofocus>
+                @error('content')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+        </div>
+    </div>
     <input type="hidden" name="id" value="{{ $article -> id }}">
     <input type="submit" value="投稿する">
 </form>
--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -4,22 +4,52 @@
 
 @section("content")
 
-<p>投稿ページ</p>
-<form action="/getRuleArticle" method="POST">
-@csrf
-    <label>ジャンルセレクト
-        <select name="category_id">
-            <option value="1">食事</option>
-            <option value="2">運動</option>
-            <option value="3">睡眠</option>
-        </select>
-    </label>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('投稿ページ') }}</div>
 
-    <p>タイトル；<input type="name" name="title"></p>
-    <p>内容；<input type="text" name="content"></p>
+                <div class="card-body">
+                    <form action="/getRuleArticle" method="POST">
+                        @csrf
+                        <div class="form-group row">
+                            <label>ジャンルセレクト
+                                <select name="category_id">
+                                    <option value="1">食事</option>
+                                    <option value="2">運動</option>
+                                    <option value="3">睡眠</option>
+                                </select>
+                            </label>
+                        </div>
 
-    <input type="submit" value="投稿する">
-</form>
+                        <div class="form-group row">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('タイトル') }}</label>
+
+                            <div class="col-md-6">
+                            <input type="text" class="form-control @error('taxt') is-invalid @enderror" name="title" required autocomplete="taxt" autofocus>
+                                @error('text')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="content" class="col-md-4 col-form-label text-md-right">{{ __('内容') }}</label>
+
+                            <div class="col-md-6">
+                            <input type="text" class="form-control @error('content') is-invalid @enderror" name="content" required autocomplete="taxt" autofocus>
+                                @error('content')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <input type="submit" value="投稿する">
+                    </form>
 <a href="/index">戻る</a>
 
 @endsection
